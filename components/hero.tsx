abe120f96ac8d3352395a811cd37715d3653a363
tomas_agence <tomas.sanhueza@sp.agence.com.br>
2024-06-25 02:43:37 -0300

    chore: Update hero images in Hero component

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,9 +4,10 @@ import { ImagesSlider } from './ui/images-slider'
 
 export function Hero() {
   const images = [
-    'https://images.unsplash.com/photo-1485433592409-9018e83a1f0d?q=80&w=1814&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1483982258113-b72862e6cff6?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1482189349482-3defd547e0e9?q=80&w=2848&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    '/images/hero/1.webp',
+    '/images/hero/2.webp',
+    '/images/hero/3.webp',
+    '/images/hero/4.webp',
   ]
   return (
     <ImagesSlider className="h-screen" images={images}>
@@ -19,6 +20,7 @@ export function Hero() {
           Desbloqueie o poder da criação de conteúdo e do marketing digital para
           fazer seu negócio crescer em Ubatuba e região.
         </p>
+
         <button className="relative mx-auto mt-4 animate-mobileActive rounded-full border border-emerald-500/20 bg-emerald-300/10 px-4 py-2 text-center text-white backdrop-blur-sm hover:bg-emerald-300/20">
           <span>Contate-nos →</span>
           <div className="absolute inset-x-0 -bottom-px mx-auto h-px w-3/4 bg-gradient-to-r from-transparent via-emerald-500 to-transparent" />
