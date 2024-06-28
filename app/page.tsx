2c7c24c6577ac28bd8ed44e7f827bc0772cfb449
tomas_agence <tomas.sanhueza@sp.agence.com.br>
2024-06-28 10:02:18 -0300

    feat: Update Home page components

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
-import { AboutUs } from '@/components/sections/about-us'
+// import { AboutUs } from '@/components/sections/about-us'
 import { Hero } from '@/components/sections/hero'
-import { InfiniteMovingCardsComponent } from '@/components/sections/infinite-moving-cards-component'
-import { OtherServices } from '@/components/sections/other-services'
-import { OurServices } from '@/components/sections/our-services'
+// import { InfiniteMovingCardsComponent } from '@/components/sections/infinite-moving-cards-component'
+// import { OtherServices } from '@/components/sections/other-services'
+// import { OurServices } from '@/components/sections/our-services'
 import { Testimonials } from '@/components/sections/testimonials'
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <InfiniteMovingCardsComponent />
+      {/* <InfiniteMovingCardsComponent />
       <AboutUs />
       <OurServices />
-      <OtherServices />
+      <OtherServices /> */}
       <Testimonials />
 
       {/* 
