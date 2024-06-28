2fa2972f577c3335c3c54d4d5d2c2b9e95b5636f
tomas_agence <tomas.sanhueza@sp.agence.com.br>
2024-06-27 22:32:46 -0300

    feat: Implement lazy loading for images in AboutUs, OurServices, Testimonials, and ContactUs components

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,15 @@
-import { AboutUs } from '@/components/sections/about-us'
-import { ContactUs } from '@/components/sections/contact-us'
 import { Hero } from '@/components/sections/hero'
-import { InfiniteMovingCardsComponent } from '@/components/sections/infinite-moving-cards-component'
-import { OtherServices } from '@/components/sections/other-services'
-import { OurServices } from '@/components/sections/our-services'
-import { Testimonials } from '@/components/sections/testimonials'
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <InfiniteMovingCardsComponent />
+      {/* <InfiniteMovingCardsComponent />
       <AboutUs />
       <OurServices />
       <OtherServices />
       <Testimonials />
-      <ContactUs />
+      <ContactUs /> */}
     </>
   )
 }
