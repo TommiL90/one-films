781695d3aaab3a635af91f63d4c50cd3d1159cab
tomas_agence <tomas.sanhueza@sp.agence.com.br>
2024-06-27 22:40:16 -0300

    feat: Add OurServices component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { AboutUs } from '@/components/sections/about-us'
 import { Hero } from '@/components/sections/hero'
 import { InfiniteMovingCardsComponent } from '@/components/sections/infinite-moving-cards-component'
+import { OurServices } from '@/components/sections/our-services'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <InfiniteMovingCardsComponent />
       <AboutUs />
+      <OurServices />
       {/* 
       <AboutUs />
       <OurServices />
